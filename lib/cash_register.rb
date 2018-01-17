7ab03cc2f1ce83e19b1df4de63d9f8923b1bf6ed
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-17 18:28:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,38 +1,32 @@
-require "pry"
+require 'pry'
 class CashRegister
+   attr_accessor :total, :discount, :items, :transaction
 
-  attr_accessor :total, :discount, :transaction, :item
-
-  def initialize(discount = 0)
-    @discount = discount
-    @total = 0
-    @items = []
-    @transaction = []
-  end
-
-  def total
-    @total
-  end
-
-  def add_item(title, price, quantity = 1)
-    @total += price * quantity
-  end
-
-  def apply_discount
-     if discount == 0
-          "There is no discount to apply."
-      else
-        discount_percentage = @discount/ 100.0
-        discount_value = discount_percentage * total
-
-        @total = total - discount_value
-       "After the discount, the total comes to $#{@total.round}."
-
-     end
+   def initialize(discount = 0)
+       @total = 0
+       @discount = discount
+       @items = []
+       @transaction = []
    end
 
-   def items
-     @items << self.add_item
-   end
+    def add_item(title, price, quantity = 1)  
+         self.total = price * quantity + total
 
-end
+         quantity.times do
+           self.items << title
+         end
+    end
+
+    def apply_discount
+      if discount == 0
+           "There is no discount to apply."
+       else
+         discount_percentage = @discount/ 100.0 
+         discount_value = discount_percentage * total
+
+         @total = total - discount_value
+        "After the discount, the total comes to $#{@total.round}."
+
+      end
+    end
+  end
